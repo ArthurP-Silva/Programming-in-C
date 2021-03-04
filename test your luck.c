4c3978eccbdaecb248416282469ea976205f5e01
Arthur Patricio Silva <79917958+ArthurP-Silva@users.noreply.github.com>
2021-03-04 00:08:01 -0300

    Add files via upload

---
+++ test your luck.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+	int x;
+	
+	
+	printf("test your luck\n");
+	printf("Choose a number for X between 0 - 10 : ");
+	scanf("%d",&x);
+	
+	if(x>=1 && x<3)
+	printf("you won a cinema ticket\n");
+	else
+	
+	
+	if(x>=3 && x<=7)
+	printf("better luck next time\n");
+	else
+	
+	
+	if(x>7 && x<=9)
+	printf("you won 1$\n");
+	else
+	
+	if(x=10 )
+	printf("you won 1 iphone 12");
+	else
+
+	
+
+	
+	
+	system("pause");
+		
+	return 0;
+	
+	
+
+
+	
+}
